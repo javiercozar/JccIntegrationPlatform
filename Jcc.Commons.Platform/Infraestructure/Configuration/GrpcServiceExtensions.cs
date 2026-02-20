c9ef3260f94e122a8cb93a2f2a27abaa9755c2e1
Javier Cozar <javier.cozar@gmail.com>
2026-02-20 13:50:19 +0100

    Add gRPC services for hotel availability search and configuration files

---
+++ Jcc.Commons.Platform/Infraestructure/Configuration/GrpcServiceExtensions.cs
@@ -0,0 +1,59 @@
+using Jcc.Commons.Platform.Application.Search.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jcc.Commons.Platform.Infraestructure.Configuration;
+
+/// <summary>
+/// Extensions for configuring gRPC services in the application.
+/// </summary>
+public static class GrpcServiceExtensions
+{
+    /// <summary>
+    /// Registers the necessary gRPC services in the dependency injection container.
+    /// </summary>
+    /// <param name="services">Collection of services.</param>
+    /// <returns>The service collection for chaining.</returns>
+    public static IServiceCollection AddGrpcServices(this IServiceCollection services)
+    {
+        if (services == null)
+            throw new ArgumentNullException(nameof(services));
+
+        // Register gRPC services
+        services.AddGrpc(options =>
+        {
+            // Configuration of default timeouts
+            options.MaxSendMessageSize = 100 * 1024 * 1024; // 100MB
+            options.MaxReceiveMessageSize = 100 * 1024 * 1024; // 100MB
+            options.ResponseCompressionAlgorithm = "gzip";
+            options.ResponseCompressionLevel = System.IO.Compression.CompressionLevel.Fastest;
+        });
+
+        // Register service implementations
+        services.AddScoped<SearchAvailabilityService>();
+
+        return services;
+    }
+
+    /// <summary>
+    /// Configures the gRPC endpoints for the application.
+    /// Should be called in the middleware pipeline configuration.
+    /// </summary>
+    /// <param name="app">The application builder.</param>
+    public static void MapGrpcServices(this WebApplication app)
+    {
+        if (app == null)
+            throw new ArgumentNullException(nameof(app));
+
+        // Map gRPC services
+        app.MapGrpcService<SearchAvailabilityService>();
+        
+        // gRPC test endpoint
+        app.MapGet("/", async context =>
+        {
+            await context.Response.WriteAsync(
+                "gRPC server is active. Use a gRPC client to communicate with the available services.");
+        });
+    }
+}
